815de4bcbd091bcb9d114db313e917006ccef926
Amresh Warad <150937631+Amresh2004@users.noreply.github.com>
2025-01-25 14:28:00 +0530

    Update and rename set a and b.c to Set A & B.c

--- OS-II/Assignment 1/Set A & B.c
+++ OS-II/Assignment 1/Set A & B.c
@@ -1,43 +1,49 @@
-
 #include<stdio.h>
 
 int n = 5, m = 3, j, i, cnt;
-int work[] = {3, 3, 2};
+int avl[] = {3, 3, 2};
 int max[7][7];
 int allo[7][7];
 int need[7][7];
-int ss[5];
+int safe[5];
 int finish[5];
 
-void print(int x[7][7]) {
+
+void print(int x[7][7]) 
+{
     int i, j;
-    for (i = 0; i < n; i++) {
+    for (i = 0; i < n; i++) 
+    {
         for (j = 0; j < m; j++)
-            printf("%d\t", x[i][j]);
-        printf("\n");
+             printf("%d\t",x[i][j]);
+	printf("\n");
     }
 }
 
-void acceptdata(int x[7][7]) {
+void acceptdata(int x[7][7]) 
+{
     int i, j;
-    for (i = 0; i < n; i++) {
+    for (i = 0; i < n; i++) 
+    {
         for (j = 0; j < m; j++)
             scanf("%d", &x[i][j]);
     }
 }
 
-void computeneed() {
+void computeneed() 
+{
     int i, j;
     for (i = 0; i < n; i++)
         for (j = 0; j < m; j++)
             need[i][j] = max[i][j] - allo[i][j];
 }
 
-int checkneed(int i) {
+int checkneed(int i) 
+{
     int j;
     cnt = 0;
     for (j = 0; j < m; j++)
-        if (need[i][j] <= work[j])
+        if (need[i][j] <= avl[j])
             cnt++;
     if (cnt == m)
         return 1;
@@ -45,78 +51,89 @@ int checkneed(int i) {
         return 0;
 }
 
-void checkstate() {
+void checkstate() 
+{
     int flag = 0;
     int k = 0, cnt = 0;
     for (i = 0; i < n; i++)
         finish[i] = 0;
-    while (1) {
+    while (1) 
+    {
         flag = 0;
-        for (i = 0; i < n; i++) {
+        for (i = 0; i < n; i++) 
+	{
             if (finish[i] == 0)
-                if (checkneed(i) == 1) {
+                if (checkneed(i) == 1) 
+		{
                     for (j = 0; j < m; j++)
-                        work[j] = work[j] + allo[i][j];
+                        avl[j] = avl[j] + allo[i][j];
                     finish[i] = 1;
                     flag = 1;
-                    ss[k++] = i;
+                    safe[k++] = i;
                 }
         }
-        if (n == k) {
+        if (n == k) 
+	{
             cnt = 1;
             break;
         }
-        if (flag == 0) {
+        if (flag == 0) 
+	{
             printf("Not in safe state\n");
             break;
         }
     }
-    if (cnt == 1) {
+    if (cnt == 1) 
+    {
+	printf("System is in safe state\n");
         printf("Safe sequence is\n");
         for (i = 0; i < n; i++)
-            printf("P%d\t", ss[i]);
+            printf("P%d\t", safe[i]);
         printf("\n");
     }
 }
 
-int requestresource(int process, int request[]) {
+int requestresource(int p, int r[]) 
+{
     int j;
     for (j = 0; j < m; j++) {
-        if (request[j] > need[process][j]) {
+        if (r[j] > need[p][j]) 
+	{
             printf("Error: Request exceeds maximum need\n");
             return 0;
         }
-        if (request[j] > work[j]) {
+        if (r[j] > avl[j]) 
+	{
             printf("Error: Resources not available\n");
             return 0;
         }
     }
-    // Temporarily allocate resources
-    for (j = 0; j < m; j++) {
-        work[j] -= request[j];
-        allo[process][j] += request[j];
-        need[process][j] -= request[j];
+    for (j = 0; j < m; j++) 
+    {
+        avl[j] -= r[j];
+        allo[p][j] += r[j];
+        need[p][j] -= r[j];
     }
 
-    // Check if the system remains in a safe state
-    int original_work[m];
+    int original_avl[m];
     for (j = 0; j < m; j++)
-        original_work[j] = work[j];
+        original_avl[j] = avl[j];
 
     checkstate();
 
-    // Restore original state if unsafe
-    for (j = 0; j < m; j++) {
-        work[j] = original_work[j];
-        allo[process][j] -= request[j];
-        need[process][j] += request[j];
+    for (j = 0; j < m; j++) 
+    {
+        avl[j] = original_avl[j];
+        allo[p][j] -= r[j];
+        need[p][j] += r[j];
     }
 
     return 1;
 }
 
-int main() {
-    int choice, process, request[7];
+int main() 
+{
+    int choice, p, r[7];
 
     printf("Allocation matrix\n");
     acceptdata(allo);
@@ -128,30 +145,32 @@ int main() {
     printf("Need matrix\n");
     computeneed();
     print(need);
-    printf("Work is\n");
+    printf("Avaialbe MAtrix is\n");
     for (i = 0; i < m; i++)
-        printf("%d\t", work[i]);
+        printf("%d\t", avl[i]);
 
-    while (1) {
+    while (1) 
+    {
         printf("\n\nChoose an option:\n");
         printf("1. Check safe state\n");
         printf("2. Request resources\n");
         printf("3. Exit\n");
         scanf("%d", &choice);
 
-        switch (choice) {
+        switch (choice) 
+	{
         case 1:
             checkstate();
             break;
         case 2:
             printf("Enter process number: ");
-            scanf("%d", &process);
-            printf("Enter resource request for process P%d:\n", process);
+            scanf("%d", &p);
+            printf("Enter resource request for process P%d:\n", p);
             for (i = 0; i < m; i++)
-                scanf("%d", &request[i]);
-            if (requestresource(process, request))
+                scanf("%d", &r[i]);
+            if (requestresource(p, r))
                 printf("Request granted\n");
-            break;
+             break;
         case 3:
             return 0;
         default:
@@ -159,3 +178,4 @@ int main() {
         }
     }
 }
+
