53651ed8222c27a3c2f25d9cf0efed95bff68737
Amresh Warad <150937631+Amresh2004@users.noreply.github.com>
2025-03-14 23:53:54 +0530

    Update insert.php

--- WT-II/Assignment 5/insert.php
+++ WT-II/Assignment 5/insert.php
@@ -5,8 +5,7 @@ $user = "postgres";
 $password = "8624807723";
 
 $conn = pg_connect("host=$host dbname=$dbname user=$user password=$password");
-
-// Check connection
+
 if (!$conn) {
     die("Connection failed: " . pg_last_error());
 }
